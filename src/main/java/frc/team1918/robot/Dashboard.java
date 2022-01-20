8254fae86c79e68eff3bbeda419cf6defd93596c
ncgears <1918ncgears@gmail.com>
2022-01-20 13:56:32 -0500

    Cleanup dash helper class

--- src/main/java/frc/team1918/robot/Dashboard.java
+++ src/main/java/frc/team1918/robot/Dashboard.java
@@ -6,30 +6,14 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  * This class reads and writes values to/from the SmartDashboard
  */
 public class Dashboard {
-    //These define the names of the controls on the dashboard
-    private static final String dash_shooter_speed = "ShootSpeed"; //Current shooter speed
-    private static final String dash_shooter_target = "Shooter Target Speed"; //Target shooter speed
-    private static final String dash_hood_pos = "HoodPosition"; //Hood position (up/down)
-    private static final String dash_gyro_angle = "GyroAngle"; //Gyro angle
-
     public static final class Shooter {
-        public static final void setCurrentSpeed(double speed) {
-            SmartDashboard.putNumber(dash_shooter_speed,speed);
-        }
-        public static final void setTargetSpeed(double speed) {
-            SmartDashboard.putNumber(dash_shooter_target,speed);
-        }
-        public static final double getTargetSpeed(double default_val) {
-            return (double) SmartDashboard.getNumber(dash_shooter_target,default_val);
-        }
-        public static final void setHoodPosition(boolean up) {
-            SmartDashboard.putString(dash_hood_pos,(up)?"up":"down");
-        }
+        public static final void setCurrentSpeed(double speed) { SmartDashboard.putNumber("ShootSpeed",speed); }
+        public static final void setHoodPosition(boolean up) { SmartDashboard.putString("HoodPosition",(up)?"up":"down"); }
+        public static final void setTargetSpeed(double speed) { SmartDashboard.putNumber("Shooter Target Speed",speed); }
+        public static final double getTargetSpeed(double default_val) { return (double) SmartDashboard.getNumber("Shooter Target Speed",default_val); }
     }
     public static final class Gyro {
-        public static final void setGyroAngle(double angle) {
-            SmartDashboard.putNumber(dash_gyro_angle,angle);
-        }
+        public static final void setGyroAngle(double angle) { SmartDashboard.putNumber("GyroAngle",angle); }
     }
     public static final class DriveTrain {
         public static final void setTurnPosition(String module, double value) { SmartDashboard.putNumber(module+" Position", value); }
