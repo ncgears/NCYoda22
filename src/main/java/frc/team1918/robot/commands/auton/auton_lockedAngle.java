8cd95562e8eac75529a3c422f60de906eadf1083
NCGears07 <coder07@ncgears.com>
2022-01-29 12:56:32 -0500

    Removed Locked Angle testing in Auton

--- src/main/java/frc/team1918/robot/commands/auton/auton_lockedAngle.java
+++
@@ -1,9 +0,0 @@
-package frc.team1918.robot.commands.auton;
-
-import frc.team1918.robot.Constants.Swerve.FL;
-import frc.team1918.robot.commands.drive.drive_lockAngle;
-
-//If we lock the angle of the swerve modules, we may be able to treat the robot like a tank drive for the auton calculation
-public class auton_lockedAngle {
-    
-}
