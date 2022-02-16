78daea9747b9ff83aad2f8d4ffd6ebec6397ca95
ncgears <1918ncgears@gmail.com>
2022-02-16 09:09:51 -0500

    Remove stopFeeder from auton

--- src/main/java/frc/team1918/robot/commandgroups/cg_auton_4BallAuto.java
+++ src/main/java/frc/team1918/robot/commandgroups/cg_auton_4BallAuto.java
@@ -55,7 +55,7 @@ public class cg_auton_4BallAuto extends SequentialCommandGroup {
         // new WaitCommand(1.25), //give time for shot
         new shooter_stopShooter(m_shooter), //stop shooter
         new shooter_hoodDown(m_shooter), //lower the hood
-        new feeder_stop(m_feeder), //stop the feeder
+        // new feeder_stop(m_feeder), //stop the feeder -- should be handled by shootAllBalls
         new collector_deployIntake(m_collector), //deploy collector
         new ParallelDeadlineGroup( //do until trajectory complete
           // new WaitCommand(3.0), //placeholder for trajectory follower
@@ -71,7 +71,7 @@ public class cg_auton_4BallAuto extends SequentialCommandGroup {
         // new WaitCommand(1.25), //give time for shot
         new shooter_stopShooter(m_shooter), //stop shooter
         new shooter_hoodDown(m_shooter), //lower the hood
-        new feeder_stop(m_feeder), //stop the feeder
+        // new feeder_stop(m_feeder), //stop the feeder -- should be handled by shootAllBalls
         new helpers_debugMessage("Auton: Done with auton")
     );
   }
