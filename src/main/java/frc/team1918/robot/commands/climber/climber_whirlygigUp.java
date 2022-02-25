2182c6b9542d7d4ff90e1c458f37a83b0d5f70db
NCGears07 <coder07@ncgears.com>
2022-02-24 20:28:27 -0500

    set isFinished to true

--- src/main/java/frc/team1918/robot/commands/climber/climber_whirlygigUp.java
+++ src/main/java/frc/team1918/robot/commands/climber/climber_whirlygigUp.java
@@ -47,6 +47,6 @@ public class climber_whirlygigUp extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return true;
   }
 }
