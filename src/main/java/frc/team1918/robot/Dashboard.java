7e3523ccbf88f6d3170ed420d296e5106d676ff6
ncgears <1918ncgears@gmail.com>
2022-03-01 00:05:16 -0500

    Update to Dashboard items locations

--- src/main/java/frc/team1918/robot/Dashboard.java
+++ src/main/java/frc/team1918/robot/Dashboard.java
@@ -7,15 +7,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class Dashboard {
     public static final class Climber {
-        public static final void setClimberDirection(String direction) { SmartDashboard.putString("ClimberDirection", direction); }
-        public static final void setClimberPosition(double position) { SmartDashboard.putNumber("ClimberPosition", position); }
-        public static final void setWhirlyPosition(String position) { SmartDashboard.putString("WhirlygigPosition", position); }
+        public static final void setClimberDirection(String direction) { SmartDashboard.putString("Climber/Direction", direction); }
+        public static final void setClimberPosition(double position) { SmartDashboard.putNumber("Climber/Position", position); }
+        public static final void setWhirlyPosition(String position) { SmartDashboard.putString("Climber/Whirlygig Position", position); }
     }
     public static final class Shooter {
-        public static final void setCurrentSpeed(double speed) { SmartDashboard.putNumber("ShootSpeed",speed); }
-        public static final void setHoodPosition(boolean up) { SmartDashboard.putString("HoodPosition",(up)?"up":"down"); }
-        public static final void setTargetSpeed(double speed) { SmartDashboard.putNumber("Shooter Target Speed",speed); }
-        public static final double getTargetSpeed(double default_val) { return (double) SmartDashboard.getNumber("Shooter Target Speed",default_val); }
+        public static final void setCurrentSpeed(double speed) { SmartDashboard.putNumber("Shooter/Shooter Speed",speed); }
+        public static final void setHoodPosition(boolean up) { SmartDashboard.putString("Shooter/Hood Position",(up)?"up":"down"); }
+        public static final void setTargetSpeed(double speed) { SmartDashboard.putNumber("Shooter/Target Speed",speed); }
+        public static final double getTargetSpeed(double default_val) { return (double) SmartDashboard.getNumber("Shooter/Target Speed",default_val); }
     }
     public static final class Gyro {
         public static final void setGyroAngle(double angle) { SmartDashboard.putNumber("GyroAngle",angle); }
