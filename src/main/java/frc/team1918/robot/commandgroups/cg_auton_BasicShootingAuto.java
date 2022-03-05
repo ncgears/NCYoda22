a24b15d498837f18cf566ad1578b25635c884a26
ncgears08 <coder08@ncgears.com>
2022-03-05 06:47:22 -0500

    Disable 2nd shot on auton

--- src/main/java/frc/team1918/robot/commandgroups/cg_auton_BasicShootingAuto.java
+++ src/main/java/frc/team1918/robot/commandgroups/cg_auton_BasicShootingAuto.java
@@ -69,15 +69,15 @@ public class cg_auton_BasicShootingAuto extends SequentialCommandGroup {
           new drive_followTrajectory(m_drive, new TwoMetersBackward()),
           new helpers_debugMessage("Auton: followTrajectory - TwoMetersBackward") //move to ball1
         ),
-        new ParallelDeadlineGroup( //do until trajectory complete
-          new SequentialCommandGroup(
-            new WaitCommand(0.5), //wait for shooter to be at speed
-            new helpers_debugMessage("Auton: Advance feeder for 1.5 seconds"),
-            new feeder_advance(m_feeder),
-            new WaitCommand(1.5)
-          ),
-          new shooter_shootNamed(m_shooter, namedShots.DEFAULT) //start the shooter with no hood
-          ),
+        // new ParallelDeadlineGroup( //do until trajectory complete
+        //   new SequentialCommandGroup(
+        //     new WaitCommand(0.5), //wait for shooter to be at speed
+        //     new helpers_debugMessage("Auton: Advance feeder for 1.5 seconds"),
+        //     new feeder_advance(m_feeder),
+        //     new WaitCommand(1.5)
+        //   ),
+        //   new shooter_shootNamed(m_shooter, namedShots.DEFAULT) //start the shooter with no hood
+        //   ),
         new helpers_debugMessage("Auton: Stop shooter and feeder"),
         new shooter_stopShooter(m_shooter), //stop shooter
         new feeder_stop(m_feeder), //stop the feeder -- should be handled by shootAllBalls
