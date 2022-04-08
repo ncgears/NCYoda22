e8ecafdd53cb03f97fa2d0ee57e1e31c3f2723ce
ncgears <1918ncgears@gmail.com>
2022-04-08 10:01:29 -0400

    Reduce hook release time from 1s to half second

--- src/main/java/frc/team1918/robot/Constants.java
+++ src/main/java/frc/team1918/robot/Constants.java
@@ -279,7 +279,7 @@ public class Constants {
         public static final boolean isSensorInverted_Motor1 = false;
         public static final boolean isSensorNotContinuous = false;
         public static final double kClimberSpeed = 1.0; //Speed at which the climber controllers operate (in fixed speed mode)
-        public static final double kHookReleaseTime = 1.0; //Time in seconds to wait before re-locking hook after unlocking
+        public static final double kHookReleaseTime = 0.50; //Time in seconds to wait before re-locking hook after unlocking
         public static final double kHookCaptureTime = 0.18; //Time to wait before hook is considered captured
         public static final double kP = 0.2;
         public static final double kI = 0.0;
